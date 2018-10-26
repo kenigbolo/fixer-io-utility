9a121671c91a2ba0d7e75266b895ab1d153292db
kenigbolo <expensivestephen@hotmail.com>
2018-10-26 15:34:56 +0300

    Update major version for release. Clean up files

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
+const queryString = require('query-string');
 /**
  * A tiny fixer io request utility.
  * This class should be used when
  * dealing with the current fixer.io api
  * At the time of this writing [2018-10-25]
  * This utility should be working.
- * Kindly raise an issue if this requires changes 
+ * Kindly raise an issue if this requires changes
  */
 class FixerIO {
   /**
@@ -14,33 +15,63 @@ class FixerIO {
    */
   constructor(apiKey) {
     if (apiKey === undefined) throw Error('Api key is a required value');
-    if (typeof apiKey !== 'string') throw TypeError('Api key should be string value');
+    if (typeof apiKey !== 'string') {
+      throw new TypeError('Api key should be string value');
+    }
     this.apiKey = apiKey;
   }
 
   /**
    * The request to fixer io
-   * @param {*} endpoint {Required} One of the avialable endpoints i.e symbol or latest
-   * @param {*} query {Optional} The api query params i.e. &base, &symbol etc.
+   * @param {String} endpoint {Required} One of the avialable
+   * endpoints i.e symbol or latest
+   * @param {String} query {Optional} The api query params
+   * i.e. &base, &symbol etc.
+   * @return {Promise} Returns a promise
    */
   request(endpoint, query = null) {
     if (endpoint == null) throw new Error('Request endpoint missing');
-    return new Promise((resolve, reject) => {
-      const url = 'http://data.fixer.io/api/' + endpoint + '?access_key=' + this.apiKey + '&';
-      const queryUrl = query != null ? url + query : url
-      http.get(queryUrl, (res) => {
-        let buffer = "";
-        res.on('data', (chunk) => {
-          buffer += chunk;
-        });
+    const url = `http://data.fixer.io/api/${endpoint}?access_key=${
+      this.apiKey
+    }`;
+    const apiUrl = query != null ? `${url}&${query}` : url;
+    return this._makeRequest(apiUrl);
+  }
 
-        res.on('end', () => {
-          return resolve(buffer);
+  /**
+   * An object query request to fixer io - called from gatsby plugin
+   * @param {String} endpoint {Required} One of the avialable
+   * endpoints i.e symbol or latest
+   * @param {Object} query {Optional} The api query params in
+   * key-value object i.e. {base=EUR} etc.
+   * @return {Promise} Returns a promise
+   */
+  reqQuery(endpoint, query = null) {
+    const url = `http://data.fixer.io/api/${endpoint}?access_key=${
+      this.apiKey
+    }`;
+    const apiUrl =
+      query != null ? `${url}&${queryString.stringify(query)}` : url;
+    return this._makeRequest(apiUrl);
+  }
+
+  _makeRequest(apiUrl) {
+    return new Promise((resolve, reject) => {
+      http
+        .get(apiUrl, (res) => {
+          let buffer = '';
+          res.on('data', (chunk) => {
+            buffer += chunk;
+          });
+
+          res.on('end', () => {
+            return resolve(JSON.parse(buffer));
+          });
+        })
+        .on('error', (err) => {
+          return reject({error: err.message});
         });
-      }).on('error', (err) => {
-        return reject({error: err.message});
-      });
-    })
+    });
   }
 }
 
